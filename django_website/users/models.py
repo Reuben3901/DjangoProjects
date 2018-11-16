13f97f2bc2f5d91f4fa391ddab7da5fff32e8da5
Reuben <reubenwyoung@gmail.com>
2018-11-15 16:12:30 -0800

    Profile picture override and delete

--- django_website/users/models.py
+++ django_website/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,6 +8,27 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+
+    # Resizing images and Overwirting the save model
+    def save(self):
+
+        # Delete old Profile Image
+        try:
+            this = Profile.objects.get(id=self.id)
+            if this.image != self.image:
+                this.image.delete(save=False)
+        except: pass # when new photo then we do nothing, normal case
+        # Overwrites the save.
+        # Grabs the the parent saved image and resizes it
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 class ProfilePictures(models.Model):
     image = models.ImageField(upload_to='profile_pics', null=True, default='profile_pics/default.jpg')
