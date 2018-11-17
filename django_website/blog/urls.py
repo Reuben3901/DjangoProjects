cc0cf08f7e110192ea4327360b0d191d879a2254
Reuben <reubenwyoung@gmail.com>
2018-11-16 19:02:30 -0800

    Create Update Delete Posts

--- django_website/blog/urls.py
+++ django_website/blog/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
-
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('class/', PostListView.as_view(), name='blog-list'),
+
+    # Shares template with update view
+    # Expects <model>_form. post_form
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
-    #path('', PostListView.as_view(), name='blog-home'),
     #path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     # this leads to blog/about
     #  No need to enter this into main URLs.PY beacause that points to the main blog url
@@ -13,7 +19,7 @@ urlpatterns = [
     #path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     # Shares template with update view
     #path('post/new/', PostCreateView.as_view(), name='post-create'),
-    #path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    #path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
 ]
