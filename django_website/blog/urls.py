833cee12f59a5e470af15189ca312fe232effa51
Reuben <reubenwyoung@gmail.com>
2018-11-20 12:31:02 -0800

    User Post List

--- django_website/blog/urls.py
+++ django_website/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
@@ -12,13 +12,12 @@ urlpatterns = [
 
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
-    #path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     # this leads to blog/about
     #  No need to enter this into main URLs.PY beacause that points to the main blog url
     #path('about/', views.about, name='blog-about'),
     #path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     # Shares template with update view
-    #path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
