63163da9bdb269b0d1475da342896719bbea014a
Reuben <reubenwyoung@gmail.com>
2018-11-28 14:15:59 -0800

    User password reset

--- django_website/django_website/settings.py
+++ django_website/django_website/settings.py
@@ -136,3 +136,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 # Overriding the login route if login_required
 LOGIN_URL = 'users-login'
+
+# Google email login
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get('Reubenwyoung@gmail.com')
+# EMAIL_HOST_PASSWORD = os.environ.get('Arnold#3901')
